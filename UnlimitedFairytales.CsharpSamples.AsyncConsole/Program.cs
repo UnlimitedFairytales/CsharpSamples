49d96c7cb8faaf3505ef0c9fdcd089254804cfdb
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-04-05 02:37:45 +0900

    fix Console Async Sample

--- UnlimitedFairytales.CsharpSamples.AsyncConsole/Program.cs
+++ UnlimitedFairytales.CsharpSamples.AsyncConsole/Program.cs
@@ -5,8 +5,15 @@ namespace UnlimitedFairytales.CsharpSamples.AsyncConsole
 {
     class Program
     {
+        static void Log(string msg = "")
+        {
+            var dateTime = DateTime.Now.ToString("HH:mm:ss.fff");
+            var threadId = "threadId=" + System.Threading.Thread.CurrentThread.ManagedThreadId;
+            System.Diagnostics.Trace.WriteLine($"{dateTime} {threadId} {msg}");
+        }
+
         // Project > build > advanced settings > Language Version > latest miner(C#7.1 <=)
-        // This is alias for Task.GetAwaiter().GetResult()
+        // "async Task Main" is alias for Task.GetAwaiter().GetResult()
         static async Task Main(string[] args)
         {
             Console.WriteLine("Press d or else.");
@@ -14,47 +21,79 @@ namespace UnlimitedFairytales.CsharpSamples.AsyncConsole
             Console.WriteLine();
             if (key.Key == ConsoleKey.D)
             {
+                /*
+                 * If raisesDeadLock mode, deadlock!
+                 * Reason
+                 * https://stackoverflow.com/questions/58960005/async-await-deadlock-when-using-windowsformssynchronizationcontext-in-a-console
+                 * 
+                 * 1. 
+                 * WindowsFormsSynchronizationContext depends on Win32 message loop.
+                 * But, console application don't start it.
+                 * Win32 message loop is started by the followings.
+                 * A. C#  Application.Run()
+                 * B. C++ LRESULT CALLBACK WndProc()
+                 * 
+                 * 2.
+                 * new Form() or other controls, switchs SynchronizationContext to WindowsFormsSynchronizationContext.
+                 * (WindowsFormsSynchronizationContext.AutoInstall = true)
+                 * 
+                 * 3. 
+                 * Program is WindowsFormsSynchronizationContext mode, but it don't start Win32 message loop.
+                 * In this state, WindowsFormsSynchronizationContext.Post() (It is called by completing of await) don't work.
+                 */
                 Console.WriteLine("Deadlock sample.");
-                Console.WriteLine(await Do1Async("1", 300, true));
-                Console.WriteLine(await Do1Async("2", 200, true));
-                Console.WriteLine(await Do1Async("3", 100, true));
+                Log("before new From : Current=" + System.Threading.SynchronizationContext.Current);
+                var frm = new System.Windows.Forms.Form();
+                Log("after  new From : Current=" + System.Threading.SynchronizationContext.Current);
+                await Task.Delay(1000); // dead lock!
             }
-            else
-            {
-                Console.WriteLine("Normal async sample.");
-                Console.WriteLine(await Do1Async("1", 300, false));
-                Console.WriteLine(await Do1Async("2", 200, false));
-                Console.WriteLine(await Do1Async("3", 100, false));
-            }
-            Console.WriteLine("Complete.");
+            Log();
+
+            Log("1st begin.");
+            await Do1Async("1st", 500, true);
+            Log("1st end.");
+            Log("2nd begin.");
+            await Do1Async("2nd", 1000, true);
+            Log("2nd end.");
+            Log("3rd begin.");
+            await Do1Async("3rd", 1500, true);
+            Log("3rd end.");
+
+            Console.WriteLine("All complete.");
             Console.ReadKey();
         }
 
-        static async Task<string> Do1Async(string no, int wait, bool raisesDeadLock)
+        static async Task Do1Async(string name, int wait, bool configureAwait)
         {
-            Console.WriteLine(no + nameof(Do1Async));
+            var self = nameof(Do1Async);
+            Log($"{name} {self} begin");
             await Task.Delay(wait);
-            return await Do2Async(no, wait, raisesDeadLock);
+            await Do2Async(name, wait, configureAwait).ConfigureAwait(configureAwait);
+            Log($"{name} {self} end");
+            return;
         }
 
-        static async Task<string> Do2Async(string no, int wait, bool raisesDeadLock)
+        static async Task Do2Async(string name, int wait, bool configureAwait)
         {
-            Console.WriteLine(no + nameof(Do2Async));
+            var self = nameof(Do2Async);
+            Log($"{name} {self} begin");
             await Task.Delay(wait);
-            return await Do3Async(no, wait, raisesDeadLock);
+            await Do3Async(name, wait, configureAwait).ConfigureAwait(configureAwait);
+            Log($"{name} {self} end");
+            return;
         }
 
-        static async Task<string> Do3Async(string no, int wait, bool raisesDeadLock)
+        static async Task Do3Async(string name, int wait, bool configureAwait)
         {
-            Console.WriteLine(no + nameof(Do3Async));
-            if (raisesDeadLock)
-            {
-                Console.WriteLine("Current=" + System.Threading.SynchronizationContext.Current);
-                var frm = new System.Windows.Forms.Form();
-                Console.WriteLine("Current=" + System.Threading.SynchronizationContext.Current);
-            }
-            await Task.Delay(wait); // if raisesDeadLock, dead lock!
-            return no + " called";
+            var self = nameof(Do3Async);
+            Log($"{name} {self} begin");
+            await Task.Run(() => {
+                Log($"{name} {self} inner task before sleep.");
+                System.Threading.Thread.Sleep(wait);
+                Log($"{name} {self} inner task after sleep.");
+            }).ConfigureAwait(configureAwait);
+            Log($"{name} {self} end");
+            return;
         }
     }
 }
