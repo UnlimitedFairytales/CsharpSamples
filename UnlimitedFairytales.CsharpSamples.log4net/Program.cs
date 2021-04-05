d22e00b6650593bd852c639f9cf7ea7678745bbe
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-04-05 16:44:22 +0900

    add log4net sample

---
+++ UnlimitedFairytales.CsharpSamples.log4net/Program.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+using System;
+
+namespace UnlimitedFairytales.CsharpSamples.log4net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = Log4netExtension.GetLogger(null, typeof(Program));
+            try
+            {
+                throw new Exception();
+            }
+            catch (Exception ex)
+            {
+                GlobalContext.Properties["UserId"] = "12345";
+                for (int i = 0; i < 100; i++)
+                {
+                    logger.Debug($"メッセージ{i}", ex);
+                }
+            }
+        }
+    }
+}
