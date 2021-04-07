e4e86aa7dbc9e97c8e8ec935f7991602a71fd4ae
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-04-07 19:02:25 +0900

    add sqlite sample

---
+++ UnlimitedFairytales.CsharpSamples.SQLite/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace UnlimitedFairytales.CsharpSamples.SQLite
+{
+    class Program
+    {
+        static readonly string DB_FILE_PATH = @"DB\sqlite.db";
+        static void Main(string[] args)
+        {
+            SQLiteConnection conn;
+            if (!File.Exists(DB_FILE_PATH))
+            {
+                conn = CreateDbFileAndOpen(DB_FILE_PATH);
+            }
+            else
+            {
+                conn = new SQLiteConnection();
+                conn.ConnectionString = $@"Data Source={DB_FILE_PATH}";
+                conn.Open();
+            }
+            
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from accounts";
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var str = "id:" + reader.GetInt32(0).ToString() + Environment.NewLine
+                    + "name:" + reader.GetString(1);
+                Console.WriteLine(str);
+            }
+            Console.ReadKey();
+        }
+
+        static SQLiteConnection CreateDbFileAndOpen(string dbFilePath)
+        {
+            var dirPath = Path.GetDirectoryName(DB_FILE_PATH);
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+            SQLiteConnection.CreateFile(dbFilePath);
+            var conn = new SQLiteConnection();
+            conn.ConnectionString = $@"Data Source={dbFilePath}";
+            conn.Open();
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "create table accounts (id INTEGER NOT NULL, name TEXT, primary key(id))";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "insert into accounts VALUES(1, 'Alice')";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "insert into accounts VALUES(2, 'Bob')";
+            cmd.ExecuteNonQuery();
+            return conn;
+        }
+    }
+}
---
+++ UnlimitedFairytales.CsharpSamples.SQLiteWithDapper/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using Dapper;
+
+namespace UnlimitedFairytales.CsharpSamples.SQLiteWithDapper
+{
+    class Program
+    {
+        static readonly string DB_FILE_PATH = @"DB\sqlite.db";
+        static void Main(string[] args)
+        {
+            SQLiteConnection conn;
+            if (!File.Exists(DB_FILE_PATH))
+            {
+                conn = CreateDbFileAndOpen(DB_FILE_PATH);
+            }
+            else
+            {
+                conn = new SQLiteConnection();
+                conn.ConnectionString = $@"Data Source={DB_FILE_PATH}";
+                conn.Open();
+            }
+
+            var results = conn.Query("select * from accounts");
+
+            foreach (var r in results)
+            {
+                Console.WriteLine($"id:{r.id}, name:{r.name}");
+            }
+            Console.ReadKey();
+        }
+
+        static SQLiteConnection CreateDbFileAndOpen(string dbFilePath)
+        {
+            var dirPath = Path.GetDirectoryName(DB_FILE_PATH);
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+            SQLiteConnection.CreateFile(dbFilePath);
+            var conn = new SQLiteConnection();
+            conn.ConnectionString = $@"Data Source={dbFilePath}";
+            conn.Open();
+            conn.Execute("create table accounts (id INTEGER NOT NULL, name TEXT, primary key(id))");
+            conn.Execute("insert into accounts VALUES(1, 'Alice')");
+            conn.Execute("insert into accounts VALUES(2, 'Bob')");
+            return conn;
+        }
+    }
+}
