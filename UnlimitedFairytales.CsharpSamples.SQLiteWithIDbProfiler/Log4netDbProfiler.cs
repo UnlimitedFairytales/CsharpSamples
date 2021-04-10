58bfd743467ccbb3d3f00884b353ba3ec8455380
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-04-11 01:07:38 +0900

    IDbProfilerサンプル追加。

---
+++ UnlimitedFairytales.CsharpSamples.SQLiteWithIDbProfiler/Log4netDbProfiler.cs
@@ -0,0 +1,55 @@
+﻿using StackExchange.Profiling.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnlimitedFairytales.CsharpSamples.SQLiteWithIDbProfiler
+{
+    class Log4netDbProfiler : IDbProfiler
+    {
+        private const string LOGGER_NAME = "SQL";
+        private Stopwatch stopwatch;
+        private string commandText;
+
+        public bool IsActive => true;
+
+        public Log4netDbProfiler()
+        {
+
+        }
+
+        public void ExecuteFinish(IDbCommand profiledDbCommand, SqlExecuteType executeType, DbDataReader reader)
+        {
+            commandText = profiledDbCommand.CommandText.Replace("\r", "").Replace("\n", " ").Trim();
+            if (executeType != SqlExecuteType.Reader)
+            {
+                stopwatch.Stop();
+                long millisec = 9999 < stopwatch.ElapsedMilliseconds ? 9999 : stopwatch.ElapsedMilliseconds;
+                this.GetLogger(LOGGER_NAME).Debug($"[{millisec,4}ms] " + commandText);
+            }
+        }
+
+        public void ExecuteStart(IDbCommand profiledDbCommand, SqlExecuteType executeType)
+        {
+            stopwatch = Stopwatch.StartNew();
+        }
+
+        public void OnError(IDbCommand profiledDbCommand, SqlExecuteType executeType, Exception exception)
+        {
+            stopwatch.Stop();
+            this.GetLogger(LOGGER_NAME).Error(profiledDbCommand.CommandText);
+        }
+
+        public void ReaderFinish(IDataReader reader)
+        {
+            stopwatch.Stop();
+            long millisec = 9999 < stopwatch.ElapsedMilliseconds ? 9999 : stopwatch.ElapsedMilliseconds;
+            this.GetLogger(LOGGER_NAME).Debug($"[{millisec,4}ms] " + commandText);
+        }
+    }
+}
---
+++ UnlimitedFairytales.CsharpSamples.SQLiteWithIDbProfiler/Program.cs
@@ -0,0 +1,70 @@
+﻿using StackExchange.Profiling.Data;
+using System;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.IO;
+
+namespace UnlimitedFairytales.CsharpSamples.SQLiteWithIDbProfiler
+{
+    class Program
+    {
+        static readonly string DB_FILE_PATH = @"DB\sqlite.db";
+        static void Main(string[] args)
+        {
+            DbConnection conn;
+            if (!File.Exists(DB_FILE_PATH))
+            {
+                conn = CreateDbFileAndOpen(DB_FILE_PATH);
+            }
+            else
+            {
+                conn = new SQLiteConnection();
+                conn.ConnectionString = $@"Data Source={DB_FILE_PATH}";
+                conn = DecorateDbConnection(conn);
+                conn.Open();
+            }
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from accounts";
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var str = "id:" + reader.GetInt32(0).ToString() + Environment.NewLine
+                        + "name:" + reader.GetString(1);
+                    Console.WriteLine(str);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static DbConnection CreateDbFileAndOpen(string dbFilePath)
+        {
+            var dirPath = Path.GetDirectoryName(DB_FILE_PATH);
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+            SQLiteConnection.CreateFile(dbFilePath);
+            DbConnection conn = new SQLiteConnection();
+            conn.ConnectionString = $@"Data Source={dbFilePath}";
+            conn = DecorateDbConnection(conn);
+            conn.Open();
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "create table accounts (id INTEGER NOT NULL, name TEXT, primary key(id))";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "insert into accounts VALUES(1, 'Alice')";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "insert into accounts VALUES(2, 'Bob')";
+            cmd.ExecuteNonQuery();
+            return conn;
+        }
+
+        static DbConnection DecorateDbConnection(DbConnection innerDbConnection)
+        {
+            var profiler = new Log4netDbProfiler();
+            var conn = new ProfiledDbConnection(innerDbConnection, profiler);
+            return conn;
+        }
+    }
+}
