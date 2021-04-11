08cc7191bea530711f5c1382064697d12e07b948
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-04-11 16:52:41 +0900

    FOrmUtilitySamplesとテストの追加。

---
+++ UnlimitedFairytales.CsharpSamples.FormUtilitySamples.Tests/MessageConstantTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace UnlimitedFairytales.CsharpSamples.FormUtilitySamples.Tests
+{
+    public static class Consts
+    {
+        public static string ERR_SYSTEM = "システムエラーが発生しました。管理者に連絡してください。";
+        public static string ERR_DB = "データベースエラーが発生しました。管理者に連絡してください。";
+        public static string ERR_DB_OPEN = "データベース接続を開く際にエラーが発生しました。";
+        public static string ERR_DB_BEGIN_TRANSACTION = "トランザクションを開始する際にエラーが発生しました。";
+        public static string ERR_DB_COMMIT = "トランザクションをコミットする際にエラーが発生しました。";
+        public static string ERR_DB_ROLLBACK = "トランザクションをロールバックする際にエラーが発生しました。";
+        public static string ERR_DB_CLOSE = "データベース接続を閉じる際にエラーが発生しました。";
+        public static string ERR_DB_SQL = "SQLエラーが発生しました。";
+        public static string ERR_NETWORK = "ネットワークエラーが発生しました。管理者に連絡してください。";
+    }
+
+    [TestClass]
+    public class MessageConstantTest
+    {
+        public static readonly MessageConstant ERR_SYSTEM =
+            new MessageConstant(MessageBoxIcon.Error, MessageBoxButtons.OK, () => Consts.ERR_SYSTEM);
+
+        public static readonly MessageConstant ERR_DB =
+            new MessageConstant(MessageBoxIcon.Error, MessageBoxButtons.OK, () => Consts.ERR_DB);
+
+        public static readonly MessageConstant ERR_DB_OPEN =
+            new MessageConstant(MessageBoxIcon.Error, MessageBoxButtons.OK, () => Consts.ERR_DB_OPEN);
+
+        public static readonly MessageConstant ERR_DB_BEGIN_TRANSACTION =
+            new MessageConstant(MessageBoxIcon.Error, MessageBoxButtons.OK, () => Consts.ERR_DB_BEGIN_TRANSACTION);
+
+        public static readonly MessageConstant ERR_DB_COMMIT =
+            new MessageConstant(MessageBoxIcon.Error, MessageBoxButtons.OK, () => Consts.ERR_DB_COMMIT);
+
+        public static readonly MessageConstant ERR_DB_ROLLBACK =
+            new MessageConstant(MessageBoxIcon.Error, MessageBoxButtons.OK, () => Consts.ERR_DB_ROLLBACK);
+
+        public static readonly MessageConstant ERR_DB_CLOSE =
+            new MessageConstant(MessageBoxIcon.Error, MessageBoxButtons.OK, () => Consts.ERR_DB_CLOSE);
+
+        public static readonly MessageConstant ERR_DB_SQL =
+            new MessageConstant(MessageBoxIcon.Error, MessageBoxButtons.OK, () => Consts.ERR_DB_SQL);
+
+        public static readonly MessageConstant ERR_NETWORK =
+            new MessageConstant(MessageBoxIcon.Error, MessageBoxButtons.OK, () => Consts.ERR_NETWORK);
+
+        [TestMethod]
+        public void TestERR_SYSTEM()
+        {
+            // Arrange
+            var msg = MessageConstantTest.ERR_SYSTEM;
+            // Act
+            // Assert
+            Assert.AreEqual("システムエラーが発生しました。管理者に連絡してください。", msg.MbMessage);
+        }
+    }
+}
---
+++ UnlimitedFairytales.CsharpSamples.FormUtilitySamples.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnlimitedFairytales.CsharpSamples.FormUtilitySamples.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnlimitedFairytales.CsharpSamples.FormUtilitySamples.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d1572b0-0205-473c-986b-4aefa3cf707b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
