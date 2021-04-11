4d25fc7164711dd6e3577d44a30a2f0e49f05745
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-04-12 02:11:02 +0900

    add ResxSample

---
+++ UnlimitedFairytales.CsharpSamples.ResxSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace UnlimitedFairytales.CsharpSamples.ResxSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"起動時のカルチャ:{Thread.CurrentThread.CurrentUICulture.DisplayName}");
+            Console.WriteLine("resxからの読み取り例：" + Resources.StringResource.ERR_SYSTEM);
+            Console.WriteLine();
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-us", false);
+            Console.WriteLine($"en-usカルチャ:{Thread.CurrentThread.CurrentUICulture.DisplayName}");
+            Console.WriteLine("resxからの読み取り例：" + Resources.StringResource.ERR_SYSTEM);
+            Console.WriteLine();
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ja-jp", false);
+            Console.WriteLine($"ja-jpカルチャ:{Thread.CurrentThread.CurrentUICulture.DisplayName}");
+            Console.WriteLine("resxからの読み取り例：" + Resources.StringResource.ERR_SYSTEM);
+            Console.WriteLine();
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("", false);
+            Console.WriteLine($"空文字列カルチャ:{Thread.CurrentThread.CurrentUICulture.DisplayName}");
+            Console.WriteLine("resxからの読み取り例：" + Resources.StringResource.ERR_SYSTEM);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
