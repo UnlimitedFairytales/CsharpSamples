b4d8ef3c2a12de8fd33f888142ecdd2f374d8f20
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-06-29 16:43:21 +0900

    configurationBuilderサンプル追加

---
+++ UnlimitedFairytales.CsharpSamples.ConfigurationBuilderSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+
+namespace UnlimitedFairytales.CsharpSamples.ConfigurationBuilderSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setting = new Setting();
+            var options = new JsonSerializerOptions();
+            options.IncludeFields = true;
+            var text = JsonSerializer.Serialize<Setting>(setting, options);
+            text = text.Replace("[", "[" + Environment.NewLine);
+            text = text.Replace("]", Environment.NewLine + "]");
+            text = text.Replace("{", "{" + Environment.NewLine);
+            text = text.Replace("}", Environment.NewLine + "}");
+            text = text.Replace(",", "," + Environment.NewLine);
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnlimitedFairytales.CsharpSamples.ConfigurationBuilderSample/Setting.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace UnlimitedFairytales.CsharpSamples.ConfigurationBuilderSample
+{
+    class Class1
+    {
+        public long Id;
+        public string Name;
+    }
+
+    class Setting
+    {
+        public readonly string StringSample;
+        public readonly int IntSample;
+        public readonly bool BoolSample;
+        public readonly string[] StringArraySample;
+        public readonly Class1 ClassSample;
+        public readonly Class1[] ClassArraySample;
+
+        public Setting(string filePath = "sample-setting.json")
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(filePath);
+            var configuration = builder.Build();
+
+            this.StringSample = configuration["StringSample"];
+            this.IntSample = int.Parse(configuration["IntSample"]);
+            this.BoolSample = bool.Parse(configuration["BoolSample"]);
+            this.StringArraySample = configuration.GetSection("StringArraySample").GetChildren().Select(x => x.Value).ToArray();
+            var kv = configuration.GetSection("ClassSample");
+            this.ClassSample = new Class1
+            {
+                Id = long.Parse(kv["Id"]),
+                Name = kv["Name"]
+            };
+            this.ClassArraySample = configuration.GetSection("ClassArraySample").GetChildren().Select(
+                cc => new Class1
+                {
+                    Id = long.Parse(cc["Id"]),
+                    Name = cc["Name"]
+                }).ToArray();
+        }
+    }
+}
