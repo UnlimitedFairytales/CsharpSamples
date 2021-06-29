bf1bf080edf27dafb4489bb5eb29c6b8d0aef911
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-06-29 18:14:20 +0900

    unzip追加

---
+++ UnlimitedFairytales.CsharpSamples.Zip/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace UnlimitedFairytales.CsharpSamples.Zip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new FileStream("./sample-image.zip", FileMode.Open, FileAccess.Read))
+            using (var zipArchive = new ZipArchive(stream, ZipArchiveMode.Read, false))
+            {
+                zipArchive.ExtractToDirectory("./unzipped/", true);
+            }
+        }
+    }
+}
