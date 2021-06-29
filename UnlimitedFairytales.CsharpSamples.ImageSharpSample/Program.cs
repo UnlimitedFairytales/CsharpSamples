e3e1ceaf6497a32a195c4d9c54fcc4a491166608
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-06-29 17:57:35 +0900

    ImageSharpのサンプル追加

---
+++ UnlimitedFairytales.CsharpSamples.ImageSharpSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using SixLabors.ImageSharp;
+using System;
+
+namespace UnlimitedFairytales.CsharpSamples.ImageSharpSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var img = Image.Load("./sample-image.png"))
+            {
+                Console.WriteLine(img.Size().ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
