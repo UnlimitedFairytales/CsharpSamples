f7cf83f74497eb8be2de7db9146f69ff24ee2950
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2022-05-25 20:20:13 +0900

    .NET6化

--- UnlimitedFairytales.CsharpSamples.AsyncForm/Form1.cs
+++ UnlimitedFairytales.CsharpSamples.AsyncForm/Form1.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UnlimitedFairytales.CsharpSamples.Common;
 
 namespace UnlimitedFairytales.CsharpSamples.AsyncForm
 {
     public partial class Form1 : Form
     {
-        static void Log(string msg = "")
-        {
-            var dateTime = DateTime.Now.ToString("HH:mm:ss.fff");
-            var threadId = "threadId=" + System.Threading.Thread.CurrentThread.ManagedThreadId;
-            System.Diagnostics.Trace.WriteLine($"{dateTime} {threadId} {msg}");
-        }
-
         public Form1()
         {
             InitializeComponent();
@@ -40,14 +34,14 @@ namespace UnlimitedFairytales.CsharpSamples.AsyncForm
         {
             var buttonName = nameof(this.button3);
             var self = nameof(button3_Click);
-            Form1.Log($"{buttonName} {self} : begin");
+            Helper.Log($"{buttonName} {self} : begin");
 
             var task = Task.WhenAll(
-                    Task.Run(() => { Form1.Log("inner task1");  System.Threading.Thread.Sleep(2000); return (new Random()).Next(1, 9) * 100;}),
-                    Task.Run(() => { Form1.Log("inner task2");  System.Threading.Thread.Sleep(2000); return (new Random()).Next(1, 9); }));
-            Form1.Log($"{self} : before await 2 parallel tasks");
+                    Task.Run(() => { Helper.Log("inner task1"); System.Threading.Thread.Sleep(2000); return (new Random()).Next(1, 9) * 100; }),
+                    Task.Run(() => { Helper.Log("inner task2"); System.Threading.Thread.Sleep(2000); return (new Random()).Next(1, 9); }));
+            Helper.Log($"{self} : before await 2 parallel tasks");
             var results = await task;
-            Form1.Log($"{self} : after await 2 parallel tasks");
+            Helper.Log($"{self} : after await 2 parallel tasks");
 
             var sum = 0;
             foreach (var result in results)
@@ -55,7 +49,7 @@ namespace UnlimitedFairytales.CsharpSamples.AsyncForm
                 sum += result;
             }
             this.label3.Text = sum + " at " + DateTime.Now.ToString("HH:mm:ss.fff");
-            Form1.Log($"{buttonName} {self} : end");
+            Helper.Log($"{buttonName} {self} : end");
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -65,46 +59,13 @@ namespace UnlimitedFairytales.CsharpSamples.AsyncForm
 
         private async Task Do1AsyncWrapper(string buttonName, string self, Label label, bool innerConfigureAwait)
         {
-            var target = $"await {nameof(Do1Async)}()";
-            Log($"{buttonName} {self} : begin");
+            var target = $"await {nameof(AsyncSample.Do1Async)}()";
+            Helper.Log($"{buttonName} {self} : begin");
             // label.Textを後続で呼び出すためUIスレッドである必要がある。
             // この呼び出しはConfigureAwait(true)である必要がある。
-            await Do1Async(buttonName, 1000, innerConfigureAwait);
+            await AsyncSample.Do1Async(buttonName, 1000, innerConfigureAwait);
             label.Text = $"{buttonName} completed at {DateTime.Now.ToString("HH:mm:ss.fff")}";
-            Log($"{buttonName} {self} : end");
-        }
-
-        static async Task Do1Async(string name, int wait, bool configureAwait)
-        {
-            var self = nameof(Do1Async);
-            Log($"{name} {self} begin");
-            await Task.Delay(wait);
-            await Do2Async(name, wait, configureAwait).ConfigureAwait(configureAwait);
-            Log($"{name} {self} end");
-            return;
-        }
-
-        static async Task Do2Async(string name, int wait, bool configureAwait)
-        {
-            var self = nameof(Do2Async);
-            Log($"{name} {self} begin");
-            await Task.Delay(wait);
-            await Do3Async(name, wait, configureAwait).ConfigureAwait(configureAwait);
-            Log($"{name} {self} end");
-            return;
-        }
-
-        static async Task Do3Async(string name, int wait, bool configureAwait)
-        {
-            var self = nameof(Do3Async);
-            Log($"{name} {self} begin");
-            await Task.Run(() => {
-                Log($"{name} {self} inner task before sleep.");
-                System.Threading.Thread.Sleep(wait);
-                Log($"{name} {self} inner task after sleep.");
-            }).ConfigureAwait(configureAwait);
-            Log($"{name} {self} end");
-            return;
+            Helper.Log($"{buttonName} {self} : end");
         }
     }
 }
--- UnlimitedFairytales.CsharpSamples.AsyncForm/Program.cs
+++ UnlimitedFairytales.CsharpSamples.AsyncForm/Program.cs
@@ -3,16 +3,17 @@ using System.Windows.Forms;
 
 namespace UnlimitedFairytales.CsharpSamples.AsyncForm
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
     }
