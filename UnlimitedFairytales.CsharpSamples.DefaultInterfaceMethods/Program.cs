713427c3786e5b9f6ea262e11811ddd10167e230
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2023-01-17 23:41:33 +0900

    インターフェースのデフォルト実装の動作確認

---
+++ UnlimitedFairytales.CsharpSamples.DefaultInterfaceMethods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+
+namespace UnlimitedFairytales.CsharpSamples.DefaultInterfaceMethods
+{
+    interface A {
+        string ThisIs() { return "This is A    "; }
+    }
+    interface B
+    {
+        string ThisIs() { return "B!!!!!!!!    "; }
+    }
+    class ImplAuto : A, B
+    {
+    }
+    class ImplImpl : A, B
+    {
+        public string ThisIs() { return "Impl!!!!!    "; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var obj = new ImplAuto();
+                Console.Write("ImplAutoの場合：");
+                // Console.Write(obj.ThisIs()); // 不可能
+                A castedA = obj;
+                Console.Write(castedA.ThisIs());
+                B castedB = obj;
+                Console.Write(castedB.ThisIs());
+                Console.WriteLine();
+            }
+            {
+                var obj = new ImplImpl();
+                Console.Write("ImplImplの場合：");
+                Console.Write(obj.ThisIs()); // OK
+                A castedA = obj;
+                Console.Write(castedA.ThisIs());
+                B castedB = obj;
+                Console.Write(castedB.ThisIs());
+                Console.WriteLine();
+            }
+        }
+    }
+}
