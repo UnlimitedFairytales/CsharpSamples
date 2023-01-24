2e91d5b06c9a197695d7058b3f319ae679f5812a
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2023-01-24 21:26:31 +0900

    json sample追加

---
+++ UnlimitedFairytales.CsharpSamples.JsonSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace UnlimitedFairytales.CsharpSamples.JsonSample
+{
+    class Staff
+    {
+        public long? StaffId { get; set; }
+        public string StaffCd { get; set; }
+        public string StaffNm { get; set; }
+    }
+    class ResultModel
+    {
+        public IList<Staff>? StaffList { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new ResultModel()
+            {
+                StaffList = new List<Staff>() {
+                    new Staff() { StaffId = 85, StaffCd ="d0000093", StaffNm="山田太郎", },
+                    new Staff() { StaffId = 96, StaffCd ="d0000104", StaffNm="山田花子", },
+                }
+            };
+            Console.WriteLine(JsonSerializer.Serialize(result));
+        }
+    }
+}
