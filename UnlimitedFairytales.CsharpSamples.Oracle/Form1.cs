9cc5594f6b810e2287a04d363c25b2a6e9a2159d
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2023-04-23 01:30:06 +0900

    メモを追記

--- UnlimitedFairytales.CsharpSamples.Oracle/Form1.cs
+++ UnlimitedFairytales.CsharpSamples.Oracle/Form1.cs
@@ -16,17 +16,33 @@ namespace UnlimitedFairytales.CsharpSamples.Oracle
             MessageBox.Show("oracle");
             var conn = new OracleConnection();
             conn.ConnectionString = "User ID=foo; Password=bar; Data Source=192.168.3.251:1521/dev";
-            conn.Open();
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = "select * from accounts";
-            using (var reader = cmd.ExecuteReader())
+            try
             {
-                while (reader.Read())
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from accounts";
+                using (var reader = cmd.ExecuteReader())
                 {
-                    var str = "id:" + reader.GetInt32(0).ToString() + Environment.NewLine
-                        + "name:" + reader.GetString(1);
-                    MessageBox.Show(str);
+                    while (reader.Read())
+                    {
+                        var str = "id:" + reader.GetInt32(0).ToString() + Environment.NewLine
+                            + "name:" + reader.GetString(1);
+                        MessageBox.Show(str);
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show(@"Linuxにログインしoracleにユーザを切り替えて各種起動していることを確認してください
+(このメッセージはフォーカスが当たっているときにCtrl+Cでコピーできます)
+su - oracle
+lsnrctl
+# start
+# quit
+sqlplus system/manager as sysdba
+# SHUTDOWN IMMEDIATE
+# STARTUP");
             }
         }
     }
