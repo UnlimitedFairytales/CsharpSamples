d52bca6178275c1ff8199a8edd471e305461db97
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2023-04-23 00:01:44 +0900

    v4.7.2ライブラリが.NET6から参照できることを確認

---
+++ UnlimitedFairytales.CsharpSamples.v472lib/ClassV472.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnlimitedFairytales.CsharpSamples.v472lib
+{
+    public class ClassV472
+    {
+        public string ThisIs { get { return "This is .net framework4.7.2's class"; } }
+    }
+}
---
+++ UnlimitedFairytales.CsharpSamples.v472libUser/Program.cs
@@ -0,0 +1,13 @@
+﻿using UnlimitedFairytales.CsharpSamples.v472lib;
+
+namespace UnlimitedFairytales.CsharpSamples.v472libUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cls = new ClassV472();
+            Console.WriteLine(cls.ThisIs);
+        }
+    }
+}
