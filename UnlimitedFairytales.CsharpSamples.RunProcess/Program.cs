d1ed31ef1ec9ad015584021bf6708decc65ec5b1
HOME-20230701\Ether <etherealgarden@msn.com>
2023-10-16 04:41:35 +0900

    RunProcessサンプルの追加

---
+++ UnlimitedFairytales.CsharpSamples.RunProcess/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace UnlimitedFairytales.CsharpSamples.RunProcess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var p = new Process())
+            {
+                var errors = new List<string>();
+                var exePath = Path.GetFullPath("..\\..\\..\\..\\UnlimitedFairytales.CsharpSamples.RunProcessInner\\bin\\Debug\\net6.0\\UnlimitedFairytales.CsharpSamples.RunProcessInner.exe");
+                p.StartInfo.WorkingDirectory = Path.GetDirectoryName(exePath);
+                p.StartInfo.FileName = exePath;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.OutputDataReceived += (sender, e) =>
+                {
+                    var temp = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(e.Data);
+                    Console.ForegroundColor = temp;
+                };
+                p.ErrorDataReceived += (sender, e) =>
+                {
+                    var temp = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Data);
+                    if (e.Data != null)
+                    {
+                        errors.Add(e.Data);
+                    }
+                    Console.ForegroundColor = temp;
+                };
+
+                p.Start();
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+                p.WaitForExit();
+
+                if (0 < errors.Count)
+                {
+                    Console.WriteLine("子プロセスでエラーがありました。");
+                    var temp = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    foreach (var error in errors)
+                    {
+                        Console.WriteLine(error);
+                    }
+                    Console.ForegroundColor = temp;
+                }
+                Console.Write($"終了コード={p.ExitCode}");
+            }
+        }
+    }
+}
---
+++ UnlimitedFairytales.CsharpSamples.RunProcessInner/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Eを押すとErrorを出します。それ以外は通常メッセージを出します。");
+var key1 = Console.ReadKey(true);
+if (key1.Key == ConsoleKey.E)
+{
+    Console.Error.WriteLine("標準Error出力");
+}
+else
+{
+    Console.WriteLine("標準出力");
+}
+
+Console.WriteLine("zを押すと終了コード1、それ以外は終了コード0で終了");
+var key2 = Console.ReadKey(true);
+if (key2.Key == ConsoleKey.Z)
+{
+    return 1;
+}
+return 0;
